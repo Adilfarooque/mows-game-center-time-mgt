7c85ac18555bd9e3b200c2ffb8758400f1c764ee
Adilfarooque <Adilfarq11@gmail.com>
2024-09-22 14:38:56 +0530

    Updated with gin

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
+const port = ":8080"
+
 func main() {
-	
+	r := gin.Default()
+	r.GET("/games", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Game list"})
+	})
+	r.Run(port)
 }
